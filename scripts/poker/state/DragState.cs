8e13fde7b79d6b59025a8caaa2e424bcfb85eb9e
Yumei1010 <161829284+Yumei1010@users.noreply.github.com>
2026-04-29 14:54:22 +0800

    feat(poker)：增加卡牌拖拽状态

---
+++ scripts/poker/state/DragState.cs
@@ -0,0 +1,53 @@
+﻿using GFrameworkGodotTemplate.scripts.enums.poker;
+using Godot;
+
+namespace GFrameworkGodotTemplate.scripts.poker.state;
+
+public partial class DragState : PokerState
+{
+    private Vector2 _lastMousePosition;
+    private float _targetRotationRad;
+    
+    public override void Process(double delta)
+    {
+        Poker.SetGlobalPosition(Poker.GetGlobalMousePosition() - Poker.GetSize() / 2);
+        
+        Vector2 currentMousePosition =  Poker.GetGlobalMousePosition();
+        Vector2 gap = currentMousePosition - _lastMousePosition;
+        _lastMousePosition = currentMousePosition;
+        _targetRotationRad = Mathf.DegToRad(Mathf.Clamp(gap.X * 15f, -30f, 30f));
+        Poker.SetRot(Mathf.LerpAngle(Poker.GetRotation(), _targetRotationRad, 10f * (float)delta));
+    }
+
+    public override void Enter()
+    {
+        // 隐藏并锁定鼠标在窗口范围内
+        Input.SetMouseMode(Input.MouseModeEnum.ConfinedHidden);
+    }
+
+    public override void Exit()
+    {
+        // 显示鼠标
+        Input.SetMouseMode(Input.MouseModeEnum.Visible);
+    }
+
+    public override void MouseDown()
+    {
+        
+    }
+
+    public override void MouseUp()
+    {
+        RequestStateChange(StateType.Idle);
+    }
+
+    public override void MouseEnter()
+    {
+        
+    }
+    
+    public override void MouseExit()
+    {
+        
+    }
+}
